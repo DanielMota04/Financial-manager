a3ac8354d52c2f351307f63aa93e0d27217e7b0c
DanielMota04 <dmota5705@gmail.com>
2025-07-29 19:26:14 -0300

    package name changed

--- src/main/java/br/com/danielmota/FinancialManagerApplication.java
+++ src/main/java/br/com/danielmota/FinancialManagerApplication.java
@@ -1,4 +1,4 @@
-package br.com.danielmota.financial_manager;
+package br.com.danielmota;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/danielmota/FinancialManagerApplicationTests.java
+++ src/test/java/br/com/danielmota/FinancialManagerApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.danielmota.financial_manager;
+package br.com.danielmota;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
