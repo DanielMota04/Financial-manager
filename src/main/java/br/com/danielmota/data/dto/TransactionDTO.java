4a81515a551e8f011783de61ff08551eed03651d
DanielMota04 <dmota5705@gmail.com>
2025-08-05 18:52:03 -0300

    service criado

--- src/main/java/br/com/danielmota/data/dto/TransactionDTO.java
+++ src/main/java/br/com/danielmota/data/dto/TransactionDTO.java
@@ -1,7 +1,81 @@
 package br.com.danielmota.data.dto;
 
+import br.com.danielmota.enums.Type;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
 public class TransactionDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    private Long id;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    private Date date;
+    private Enum<Type> type;
+    private String title;
+    private double value;
+    private String Description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Enum<Type> getType() {
+        return type;
+    }
+
+    public void setType(Enum<Type> type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TransactionDTO that = (TransactionDTO) o;
+        return Double.compare(value, that.value) == 0 && Objects.equals(id, that.id) && Objects.equals(date, that.date) && Objects.equals(type, that.type) && Objects.equals(title, that.title) && Objects.equals(Description, that.Description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, date, type, title, value, Description);
+    }
 }
--- src/main/java/br/com/danielmota/services/TransactionService.java
+++ src/main/java/br/com/danielmota/services/TransactionService.java
@@ -1,15 +1,68 @@
 package br.com.danielmota.services;
 
+import br.com.danielmota.data.dto.TransactionDTO;
+import br.com.danielmota.model.Transaction;
 import br.com.danielmota.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import static br.com.danielmota.mapper.ObjectMapper.parseListObjects;
+import static br.com.danielmota.mapper.ObjectMapper.parseObject;
+
+import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class TransactionService {
+    private Logger logger = Logger.getLogger(TransactionService.class.getName());
 
     @Autowired
     TransactionRepository repository;
 
+    public List<TransactionDTO> findAll(){
+        logger.info("Finding all transactions");
 
+        return parseListObjects(repository.findAll(), TransactionDTO.class);
+    }
+
+    public TransactionDTO findById(Long Id) throws Exception {
+        logger.info("Finding transaction by id");
+
+        var entity = repository.findById(Id)
+                .orElseThrow(() -> new Exception("transaction not found for this ID"));
+
+        return parseObject(entity, TransactionDTO.class);
+    }
+
+    public TransactionDTO create(TransactionDTO transaction){
+        logger.info("Creating transaction");
+
+        var entity = parseObject(transaction, Transaction.class);
+
+        return parseObject(repository.save(entity), TransactionDTO.class);
+    }
+
+    public TransactionDTO update(TransactionDTO transaction) throws Exception {
+        logger.info("updating transaction");
+
+        var entity = repository.findById(transaction.getId())
+                .orElseThrow(() -> new Exception("Transaction not found for this ID"));
+
+        entity.setTitle(transaction.getTitle());
+        entity.setDate(transaction.getDate());
+        entity.setDescription(transaction.getDescription());
+        entity.setType(transaction.getType());
+        entity.setValue(transaction.getValue());
+
+        return parseObject(repository.save(entity), TransactionDTO.class);
+    }
+
+    public void delete(Long id) throws Exception {
+        logger.info("deleting transaction");
+
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new Exception("Transaction not found for this ID"));
+
+        repository.delete(entity);
+    }
 
 }
